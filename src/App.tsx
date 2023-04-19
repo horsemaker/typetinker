0f4f9758dea4cd63f1a7ae14fd0dbc3f40e52221
Yash Ghodekar <horsemaker.code@gmail.com>
2023-04-20 00:32:42 +0530

    event props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import Button from "./components/Button";
 import Greet from "./components/Greet";
 import Heading from "./components/Heading";
+import Input from "./components/Input";
 import Oscar from "./components/Oscar";
 import Person from "./components/Person";
 import PersonList from "./components/PersonList";
@@ -33,6 +35,12 @@ const App = () => {
         <Heading>Oscar goes to Leonardo DiCaprio!</Heading>
       </Oscar>
       <Greet name="Yash" isLoggedIn={false} />
+      <Button
+        handleClick={(event, id) => {
+          console.log("Button Clicked", event, id);
+        }}
+      />
+      <Input value="" handleChange={(event) => console.log(event)} />
     </div>
   );
 };
